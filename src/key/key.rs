76bf808ec23bf604db5f2d84dbbdecc06e80886d
Lorenzo Maturano <maturanolorenzo@gmail.com>
2022-07-03 15:46:40 -0300

    refactor: should throw error when input has invalid digit (not a hex)

--- src/key/key.rs
+++ src/key/key.rs
@@ -1,16 +1,15 @@
-pub use crate::utils::ToByteArray;
+use crate::utils::ToByteArray;
 
+#[derive(Debug, PartialEq)]
 pub struct Key {
-    bytes: Vec<u8>
+    bytes: Vec<u8>,
 }
 
 impl Key {
-    fn new(key_as_str: &str) -> Self {
-        Key {
-            bytes: key_as_str
-                    .to_string()
-                    .to_byte_array()
-        }
+    fn new(key_as_str: &str) -> Result<Self, hex::FromHexError> {
+        let bytes = key_as_str.to_string().to_byte_array()?;
+
+        Ok(Key { bytes })
     }
 }
 
@@ -20,7 +19,16 @@ mod key_tests {
 
     #[test]
     fn test_constructor() {
-        assert_eq!(Key::new("00").bytes, vec![0x00]);
-        assert_eq!(Key::new("012345").bytes, vec![0x01, 0x23, 0x45])
+        assert_eq!(Key::new("0").unwrap().bytes, vec![0x00]);
+        assert_eq!(Key::new("00").unwrap().bytes, vec![0x00]);
+        assert_eq!(Key::new("012345").unwrap().bytes, vec![0x01, 0x23, 0x45])
+    }
+
+    #[test]
+    fn should_throw_error_if_invalid_character() {
+        assert_eq!(
+            Key::new("0123456789abcdefg"),
+            Err(hex::FromHexError::InvalidHexCharacter { c: 'g', index: 17 })
+        )
     }
 }
