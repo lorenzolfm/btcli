3be502234f49ac29aebf859a308e417daabbeef7
Lorenzo Maturano <maturanolorenzo@gmail.com>
2022-07-04 08:05:49 -0300

    refactor: renaming new -> from_str

--- src/key/key.rs
+++ src/key/key.rs
@@ -17,7 +17,7 @@ impl Key {
     /// # Arguments
     ///
     /// * `key_as_str` - A string slice that holds the hexadecimal representation of the key
-    pub fn new(key_as_str: &str) -> Result<Self, hex::FromHexError> {
+    pub fn from_str(key_as_str: &str) -> Result<Self, hex::FromHexError> {
         let bytes = key_as_str.to_string().to_byte_array()?;
 
         Ok(Key { bytes })
@@ -36,15 +36,15 @@ mod key_tests {
 
     #[test]
     fn test_constructor() {
-        assert_eq!(Key::new("0").unwrap().bytes, vec![0x00]);
-        assert_eq!(Key::new("00").unwrap().bytes, vec![0x00]);
-        assert_eq!(Key::new("012345").unwrap().bytes, vec![0x01, 0x23, 0x45])
+        assert_eq!(Key::from_str("0").unwrap().bytes, vec![0x00]);
+        assert_eq!(Key::from_str("00").unwrap().bytes, vec![0x00]);
+        assert_eq!(Key::from_str("012345").unwrap().bytes, vec![0x01, 0x23, 0x45])
     }
 
     #[test]
     fn should_throw_error_if_invalid_character() {
         assert_eq!(
-            Key::new("0123456789abcdefg"),
+            Key::from_str("0123456789abcdefg"),
             Err(hex::FromHexError::InvalidHexCharacter { c: 'g', index: 17 })
         )
     }
@@ -52,7 +52,7 @@ mod key_tests {
     #[test]
     fn should_return_bytes_encoded_as_hex_string() {
         let expected = PRIVATE_KEY.to_string();
-        let actual = Key::new(PRIVATE_KEY).unwrap().as_hex_string();
+        let actual = Key::from_str(PRIVATE_KEY).unwrap().as_hex_string();
 
         assert_eq!(expected, actual);
     }
