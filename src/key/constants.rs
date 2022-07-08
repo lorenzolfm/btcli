76a355ddbb61c80fa6ffa1171429b2bc6e749ec7
Lorenzo Maturano <maturanolorenzo@gmail.com>
2022-07-08 07:31:27 -0300

    feat: added public key constants for testing

--- src/key/constants.rs
+++ src/key/constants.rs
@@ -4,3 +4,6 @@ pub const PRIVATE_KEY: &str = "1e99423a4ed27608a15a2616a2b0e9e52ced330ac530edcc3
 pub const COMPRESSED_PRIVATE_KEY: &str = "1e99423a4ed27608a15a2616a2b0e9e52ced330ac530edcc32c8ffc6a526aedd01";
 pub const WIF: &str = "5J3mBbAH58CpQ3Y5RNJpUKPE62SQ5tfcvU2JpbnkeyhfsYB1Jcn";
 pub const COMPRESSED_WIF: &str = "KxFC1jmwwCoACiCAWZ3eXa96mBM6tb3TYzGmf6YwgdGWZgawvrtJ";
+
+pub const COMPRESSED_PUBLIC_KEY: &str = "03f028892bad7ed57d2fb57bf33081d5cfcf6f9ed3d3d7f159c2e2fff579dc341a";
+pub const UNCOMPRESSED_PUBLIC_KEY: &str = "04f028892bad7ed57d2fb57bf33081d5cfcf6f9ed3d3d7f159c2e2fff579dc341a07cf33da18bd734c600b96a72bbc4749d5141c90ec8ac328ae52ddfe2e505bdb";
