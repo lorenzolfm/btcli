d64bbc8953cdf6c70fa44f11e477f9e6244a2512
Lorenzo Maturano <maturanolorenzo@gmail.com>
2022-07-26 07:58:34 -0300

    feat: adding INVALID_PRIVATE_KEY const

--- src/key/constants.rs
+++ src/key/constants.rs
@@ -1,6 +1,7 @@
 pub const N: &str = "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141";
 
 pub const PRIVATE_KEY: &str = "1e99423a4ed27608a15a2616a2b0e9e52ced330ac530edcc32c8ffc6a526aedd";
+pub const INVALID_PRIVATE_KEY: &str = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
 pub const COMPRESSED_PRIVATE_KEY: &str = "1e99423a4ed27608a15a2616a2b0e9e52ced330ac530edcc32c8ffc6a526aedd01";
 pub const WIF: &str = "5J3mBbAH58CpQ3Y5RNJpUKPE62SQ5tfcvU2JpbnkeyhfsYB1Jcn";
 pub const COMPRESSED_WIF: &str = "KxFC1jmwwCoACiCAWZ3eXa96mBM6tb3TYzGmf6YwgdGWZgawvrtJ";
